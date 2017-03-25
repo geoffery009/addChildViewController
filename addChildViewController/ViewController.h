0e5a7def0767bc409c4954ef400b1e86caab30df
admin <admin@nicks-macbook-pro.local>
2017-03-25 10:49:18 +0800

    Initial Commit

---
+++ addChildViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  addChildViewController
+//
+//  Created by admin on 2017/3/25.
+//  Copyright © 2017年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ addChildViewController/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  addChildViewController
+//
+//  Created by admin on 2017/3/25.
+//  Copyright © 2017年 test. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
